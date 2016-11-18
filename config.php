045e0e518761e3dd279aec4adc378f81968ad72a
Nikki Rufiansya <nikkirufiansya88@gmail.com>
2016-11-18 14:11:05 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$host="sql203.phpnet.us";
+$user="pn_19170882";
+$pass="qwerty12345";
+$db="pn_19170882_crack";
+
+$kon=mysql_connect($host,$user,$pass) OR DIE ('Koneksi Gagal');
+mysql_select_db($db);
+?>
